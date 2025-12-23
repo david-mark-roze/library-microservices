6846845197386f701eca81322fe086fa29ad1afb
David Roze <droze@cirrustech.com.au>
2025-12-23 18:44:23 +1100

    WIP service registry/eureka changed

---
+++ service-registry/src/test/java/au/com/library/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.library.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
