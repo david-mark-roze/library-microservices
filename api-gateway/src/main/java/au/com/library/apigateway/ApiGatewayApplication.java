91b89812630b0e495f9c72a69d713c6577802e57
David Roze <droze@cirrustech.com.au>
2026-01-05 14:31:24 +1100

    Changed api-gateway to use application.yml instead of properties

--- api-gateway/src/main/java/au/com/library/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/au/com/library/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,10 @@ package au.com.library.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
